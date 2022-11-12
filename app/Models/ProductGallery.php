42003100056c401f747b4022e992acde8c18766e
project_gamend <gamendrachasbullah97@gmail.com>
2022-11-12 23:12:26 +0700

    first commit

---
+++ app/Models/ProductGallery.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Support\Facades\Storage;
+use League\CommonMark\Extension\CommonMark\Node\Inline\Strong;
+
+class ProductGallery extends Model
+{
+    use HasFactory, SoftDeletes;
+
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var string[]
+     */
+    protected $fillable = [
+        'products_id',
+        'url',
+    ];
+
+    public function getUrlAttribute($url)
+    {
+        return config('app.url') . Storage::url($url);
+
+    }
+}
---
+++ app/Models/TransactionItem.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class TransactionItem extends Model
+{
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var string[]
+     */
+    protected $fillable = [
+        'users_id',
+        'product_id',
+        'transaction_id',
+        'quantity'
+    ];
+
+    public function product(){
+        return $this->hasOne(Product::class, 'id', 'products_id');
+    }
+}
